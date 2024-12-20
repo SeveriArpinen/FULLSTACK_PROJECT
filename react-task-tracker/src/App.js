5de10b3a080f38af136ff4bef5793be681688050
SeveriArpinen <severi.arpinen03@gmail.com>
2024-12-20 14:45:33 +0200

    Added some functionality to the app

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,42 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors appointment",
+      day: "Dec 20th at 14:00",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Doctors appointment",
+      day: "Dec 20th at 14:00",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Doctors appointment",
+      day: "Dec 20th at 14:00",
+      reminder: false,
+    },
+  ]);
+
+  // DELETE TASK
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
